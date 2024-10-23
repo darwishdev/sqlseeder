ed254af53570539fc4dea11fef32a0d2236bcd83
darwishdev <a.darwish.dev@gmail.com>
2024-10-23 19:13:40 +0300

    fix null values single qoute

--- generator.go
+++ generator.go
@@ -94,7 +94,10 @@ func (g *Generator) GenerateRootTableDataRow(rootColumns []string, row map[strin
 	rootRow := make(map[string]interface{})
 	for _, rootColumn := range rootColumns {
 		var err error
-		value := g.Adapter.WrapWithSingleQoute(row[rootColumn].(string))
+		value := row[rootColumn].(string)
+		if value != "null" && value != "NULL" {
+			value = g.Adapter.WrapWithSingleQoute(row[rootColumn].(string))
+		}
 		isOneToMany := g.Adapter.IsOneToMany(rootColumn)
 		if isOneToMany {
 			value, err = g.GenerateOneToManySubquery(rootColumn, tableName, row[rootColumn].(string))
